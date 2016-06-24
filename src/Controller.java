8cd0cc71321f199e90056f964dcf4aba5f397906
novas <zhuzibaijiamojia@163.com>
2016-06-24 11:07:41 +0800

    修改属性获取服务

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.*;
 
 /**
@@ -7,14 +6,40 @@ import java.net.*;
  */
 //这个类是用来控制用户请求的，用户请求消息协议为PARAMS_1或者PARAMS_2_fcm
     //1表示请求支持的所有方法，2表示请求的具体方法，后面fcm表示算法的具体名称
+    //args表示的参数为本机ip 和端口号
 public class Controller {
     static ServerSocket serverSocket;
-
+    static int port;
+    static String ip;
     public static void main(String[] args)
     {
+        ip=args[0];
+        port=Integer.parseInt(args[1]);
+        File file=new File("/etc/profile");
+        try {
+            BufferedReader br=new BufferedReader(new FileReader(file));
+            String line=br.readLine();
+            while (line!=null)
+            {
+                if(line.contains("NOVAS_HOME"))
+                {
+                    String[] var=line.split("=");
+                    Constants.jarPath=var[1]+"/jar/algocenter.jar";
+                    break;
+                }
+                line=br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Constants.jarPath);
         try {
             serverSocket=new ServerSocket();
-            SocketAddress socketAddress=new InetSocketAddress(InetAddress.getByName("192.168.1.150"),9081);
+            InetAddress address=InetAddress.getLocalHost();
+            System.out.println(address.getHostAddress());
+            SocketAddress socketAddress=new InetSocketAddress(InetAddress.getByName(ip),port);
             serverSocket.bind(socketAddress);
             while (true)
             {
@@ -29,5 +54,6 @@ public class Controller {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
